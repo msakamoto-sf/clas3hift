2a8d35b8374a6eaa0c3434fb6887d86cdc128aa7
sakamoto.gsyc.3s@gmail.com <sakamoto.gsyc.3s@gmail.com@99accd7e-b654-11de-81bc-59952d0011f9>
2011-05-02 00:09:12 +0000

    - add Maven2 directories, files (work in progress)

---
+++ trunk/src/main/java/junit/extensions/clas3hift/App.java
@@ -0,0 +1,13 @@
+package junit.extensions.clas3hift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
